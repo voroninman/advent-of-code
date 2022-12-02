57abda68b0dc7c0798facbceff6d9995d59ad194
Viktor Voronin <viktor@billogram.com>
2022-12-02 17:49:26 +0100

    dayN

---
+++ 2022/day_N/solve.jl
@@ -0,0 +1,10 @@
+
+
+solve_1(input) = input |> readlines
+solve_2(input) = input |> readlines
+
+println(solve_1("input_test.txt"))
+println(solve_1("input.txt"))
+
+println(solve_2("input_test.txt"))
+println(solve_2("input.txt"))
