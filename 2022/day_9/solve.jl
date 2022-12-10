87b96906b0d05db43b1091b713227b396f28ec60
Viktor Voronin <viktor@billogram.com>
2022-12-10 02:48:53 +0100

    day9_2

--- 2022/day_9/solve.jl
+++ 2022/day_9/solve.jl
@@ -8,12 +8,7 @@ parse_cmds(input) =
     input |> readlines .|> split .|> (((cmd, n),) -> (cmd, parse(Int, n)))
 is_close(distance) = distance |> (((i, j),) -> -1 <= i <= 1 && -1 <= j <= 1)
 left, right, up, down = (0, -1), (0, 1), (-1, 0), (1, 0)
-left_up = left .+ up
-left_down = left .+ down
-right_up = right .+ up
-right_down = right .+ down
 cmd_mapping = Dict("U" => up, "R" => right, "D" => down, "L" => left)
-
 find(state, char) = Tuple(findfirst(in.(char, state)))
 
 function jump(state, char, to)
@@ -50,28 +45,25 @@ function move(state, direction)
         tail = find(state, tail_char)
         distance = new_head .- tail
 
-        # display_state_full(state)
-        # println("Move $(!is_close(distance)) tail:$tail_char@$tail to head:$head_char@$head at $distance")
-
         if !is_close(distance)
-
-            if direction == down && distance == down .+ right_down
-                direction = direction .+ right
-            elseif direction == down && distance == down .+ left_down
-                direction = direction .+ left
-            elseif direction == up && distance == up .+ right_up
-                direction = direction .+ right
-            elseif direction == up && distance == up .+ left_up
-                direction = direction .+ left
-            elseif direction == left && distance == left .+ left_down
-                direction = direction .+ down
-            elseif direction == left && distance == left .+ left_up
-                direction = direction .+ up
-            elseif direction == right && distance == right .+ right_down
-                direction = direction .+ down
-            elseif direction == right && distance == right .+ right_up
-                direction = direction .+ up
-            end
+            direction = Dict(
+                (-2, 1) => (-1, 1),
+                (1, 2) => (1, 1),
+                (0, 2) => (0, 1),
+                (2, 2) => (1, 1),
+                (-1, 2) => (-1, 1),
+                (2, -1) => (1, -1),
+                (-2, 2) => (-1, 1),
+                (-2, -1) => (-1, -1),
+                (1, -2) => (1, -1),
+                (0, -2) => (0, -1),
+                (2, 0) => (1, 0),
+                (2, -2) => (1, -1),
+                (-1, -2) => (-1, -1),
+                (-2, 0) => (-1, 0),
+                (2, 1) => (1, 1),
+                (-2, -2) => (-1, -1),
+            )[distance]
 
             state = jump(state, tail_char, tail .+ direction)
         end
@@ -79,58 +71,20 @@ function move(state, direction)
     state
 end
 
-function display_state(state)
-    function top_char(chars)
-        for char in "$order#."
-            if char in chars
-                return char
-            end
-        end
-    end
-
-    state .|>
-        (x -> top_char(x)) |>
-        eachrow .|>
-        (x -> join(x, " ")) |>
-        (x -> join(x, "\n")) |>
-        (x -> (println(repeat('-', 80)); println(x)))
-end
-
-function display_state_full(state)
-    state .|>
-        join .|>
-        (x -> lpad(x, (state .|> length |> maximum) + 1, ' ')) |>
-        eachrow .|>
-        (x -> join(x, " ")) |>
-        (x -> join(x, "\n")) |>
-        (x -> (println(repeat('-', 80)); println(x)))
-end
-
 state = to_matrix("""
-.....H
-.....1
-.....2
-.....3
-....54
-...6..
-..7...
-.8....
-9.....
+...
+...
+H..
 """)
-display(state)
-display_state(state)
 
-# state[end,1] = collect("H123456789#.")
+state[end,1] = collect("H123456789#.")
 
-for (cmd_dir, cmd_times) in parse_cmds("input_test.txt")
+for (cmd_dir, cmd_times) in parse_cmds("input.txt")
     for i in 1:cmd_times
         direction = cmd_mapping[cmd_dir]
         global state = move(state, direction)
-        display_state(state)
     end
 end
-# display_state(state)
 println(state .|> (x -> '#' in x) |> sum)
-
 
 # 2418 is too low.
