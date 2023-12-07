b9a739320e6ae3672318fe808d71ede388adb03d
Viktor Voronin <voroninman@gmail.com>
2023-12-07 08:24:59 +0100

    Still can't crack day 5 part 2

--- 2023/day_05/solve.py
+++ 2023/day_05/solve.py
@@ -1,7 +1,9 @@
+import enum
 import re
 from collections import defaultdict
 from pathlib import Path
 from pprint import pprint
+from types import new_class
 
 dir = Path(__file__).parent
 
@@ -22,20 +24,27 @@ def solve1(input_file):
         needle = seed
         for mappings in layers:
             for mapping in mappings:
-                source_start = mapping[1]
-                destination_start = mapping[0]
+                from_start = mapping[1]
+                to_start = mapping[0]
                 range_length = mapping[2]
-                source_end = source_start + range_length - 1
-                offset = needle - source_start
-                if source_start <= needle <= source_end:
-                    new_needle = destination_start + offset
+                from_end = from_start + range_length - 1
+                offset = needle - from_start
+                if from_start <= needle <= from_end:
+                    new_needle = to_start + offset
                     needle = new_needle
                     break
         minimum = min(minimum, needle)
     return minimum
 
 
-print(solve1("example.txt"))
-print(solve1("input.txt"))
-# print(solve2("example.txt"))
-# print(solve2("input.txt"))
+def solve2(input_file):
+    data = open(dir / input_file).read().strip()
+    seeds = data[7 : data.find("\n")].split(" ")
+
+    layers = []
+
+
+# print(solve1("example.txt"))
+# print(solve1("input.txt"))
+print(solve2("example.txt"))
+print(solve2("input.txt"))
